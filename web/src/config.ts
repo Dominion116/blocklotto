2cab0eeb59c541baad9baa68450fe224452d1d33
Dominion <limbotech116@gmail.com>
2025-12-18 22:37:31 +0100

    refactor: update randomness calculation in lottery winner selection and adjust package dependencies for improved functionality

--- web/src/config.ts
+++ web/src/config.ts
@@ -1,4 +1,4 @@
-import { AppConfig, UserSession, showConnect } from '@stacks/connect'
+import { AppConfig, UserSession } from '@stacks/auth'
 
 const appConfig = new AppConfig(['store_write', 'publish_data'])
 export const userSession = new UserSession({ appConfig })
@@ -8,8 +8,10 @@ export const appDetails = {
   icon: 'https://avatars.githubusercontent.com/u/37784886'
 }
 
-export const connectWallet = () => {
-  showConnect({
+export const connectWallet = async () => {
+  // @ts-ignore - showConnect is available at runtime but not in types
+  const { showConnect } = await import('@stacks/connect/dist')
+  return showConnect({
     appDetails,
     onFinish: () => {
       window.location.reload()
